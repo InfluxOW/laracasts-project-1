9717fc0bd1e158d758926458976c2dc4bd090a45
InfluxOW <krochak_n@mail.ru>
2020-05-05 21:13:30 +1000

    prepare for heroku deploy

--- app/Http/Requests/UserValidation.php
+++ app/Http/Requests/UserValidation.php
@@ -25,11 +25,19 @@ class UserValidation extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'string', 'max:255', 'min:3'],
-            'username' => ['required', 'string', 'max:30', Rule::unique('users')->ignore($this->user), 'alpha_dash', 'min:3'],
-            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($this->user)],
+            'name' => [
+                'required', 'string', 'max:255', 'min:3'
+            ],
+            'username' => [
+                'required', 'string', 'max:30', Rule::unique('users')->ignore($this->user), 'alpha_dash', 'min:3'
+            ],
+            'email' => [
+                'required', 'string', 'email', 'max:255', Rule::unique('users')->ignore($this->user)
+            ],
             // 'password' => ['required', 'string', 'min:8', 'confirmed', 'max:255'],
-            'description' => ['string', 'nullable', 'max:1000', 'min:10']
+            'description' => [
+                'string', 'nullable', 'max:1000', 'min:10'
+            ]
         ];
     }
 }
