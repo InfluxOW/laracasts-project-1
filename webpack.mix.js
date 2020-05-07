271d81106decdb751829ce7c07929c10b9ed7fd0
InfluxOW <krochak_n@mail.ru>
2020-05-07 23:41:44 +1000

    fix tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,4 @@
 const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -19,8 +18,6 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/list.scss', 'public/css')
     .copy('resources/css/menu.css', 'public/css');
-
-mix.sass('resources/sass/main.scss', 'public/css')
-    .options({
-        postCss: [ tailwindcss('tailwind.config.js') ],
-    });
+mix.postCss('resources/css/main.css', 'public/css', [
+    require('tailwindcss'),
+]);
